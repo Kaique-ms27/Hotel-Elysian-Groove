eb038dc025004d2038430837bc95f43196828a2b
Kaique Magalhães Santos <kaiquemagalhaes27@gmail.com>
2025-08-11 22:10:20 -0300

    .

---
+++ static/js/reservas.js
@@ -0,0 +1,5 @@
+document.querySelector(".reserva-total-1").textContent = document.querySelector(".reserva-qtd-1").textContent * document.querySelector(".reserva-unitario-1").textContent
+document.querySelector(".reserva-total-2").textContent = document.querySelector(".reserva-qtd-2").textContent * document.querySelector(".reserva-unitario-2").textContent
+document.querySelector(".reserva-total-3").textContent = document.querySelector(".reserva-qtd-3").textContent * document.querySelector(".reserva-unitario-3").textContent
+document.querySelector(".reserva-total-4").textContent = document.querySelector(".reserva-qtd-4").textContent * document.querySelector(".reserva-unitario-4").textContent
+document.querySelector(".reserva-total-5").textContent = document.querySelector(".reserva-qtd-5").textContent * document.querySelector(".reserva-unitario-5").textContent
